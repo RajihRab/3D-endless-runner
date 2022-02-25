8614a6b4868e054a218dc63026dd69c665cf1723
RajihRab <rajihrabbani@student.telkomuniversity.ac.id>
2022-02-25 10:31:26 +0700

    Add Project baru
    ganti project baru

---
+++ Assets/Sricpt/PlayerController.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Animator animasi;
+    public float jumpforce;
+    Rigidbody rig;
+    Transform trans;
+    public float moveSpeed = 5;
+    public float leftRightSpeed = 4;
+    Collider coll;
+    bool active = true;
+    bool grounded;
+    float up;
+    
+
+    void Start()
+    {
+        animasi = this.GetComponent<Animator>();
+        rig = GetComponent<Rigidbody>();
+        trans = GetComponent<Transform>();
+        coll = GetComponent<Collider>();
+        up = 0.085f;
+    }
+
+
+
+
+    void Update()
+    {
+
+        if (Physics.Raycast(transform.position + new Vector3(0.1f, 0.05f, 0f), Vector3.down, up)
+          || Physics.Raycast(transform.position + new Vector3(0.1f, 0.05f, 0f), Vector3.down, up)
+          || Physics.Raycast(transform.position + new Vector3(-0.1f, 0.05f, 0f), Vector3.down, up)
+          || Physics.Raycast(transform.position + new Vector3(-0.1f, 0.05f, 0f), Vector3.down, up))
+        {
+            grounded = true;
+            coll.material.dynamicFriction = 1;
+            coll.material.staticFriction = 1;
+            animasi.SetBool("grounded", true);
+        }
+        else
+        {
+            grounded = false;
+            coll.material.dynamicFriction = 0;
+            coll.material.staticFriction = 0;
+            animasi.SetBool("grounded", false);
+        }
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
+            {
+                trans.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+                animasi.Play("kiri");
+                
+                
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            {
+                trans.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+                animasi.Play("kanan");
+                
+                
+            }
+
+        }
+            
+            if (Input.GetButtonDown("Jump") && grounded)
+            
+        {
+                     animasi.Play("jump");
+                     
+                     rig.AddForce(Vector3.up * 4f, ForceMode.Impulse);
+                     
+             
+
+            
+            
+        }
+
+            
+    }
+
+
+
+   
+}
